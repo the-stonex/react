ef4531cacf00328db871ae7a3c868acb3f6787d2
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2025-03-06 23:58:24 +0530

    Update ~reaction.py

--- nexichat/modules/~reaction.py
+++ nexichat/modules/~reaction.py
@@ -1,8 +1,8 @@
-"""from pyrogram import Client, filters
+from pyrogram import Client, filters
 from pyrogram.types import Message
 from nexichat import nexichat
 
-@nexichat.on_message(filters.incoming)
+"""@nexichat.on_message(filters.incoming)
 async def react_to_messages(client: Client, message: Message):
     try:
         await message.react("👍")
